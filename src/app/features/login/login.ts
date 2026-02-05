c38c76ef8a87124be724748f3a55272234cb1036
nachibat <nachi2083@gmail.com>
2026-02-04 22:05:54 -0300

    Se crea vista de login

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+    selector: 'app-login',
+    imports: [
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatDividerModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
+    templateUrl: './login.html',
+    styleUrl: './login.css',
 })
 export class Login {
+    hidePassword = signal(true);
+    loginForm: FormGroup;
 
+    constructor(private fb: FormBuilder) {
+        this.loginForm = this.fb.group({
+            username: ['', [Validators.required]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            rememberMe: [false],
+        });
+    }
+
+    onSubmit() {
+        if (this.loginForm.invalid) return;
+        console.log(this.loginForm.controls['username'].value);
+        console.log(this.loginForm.controls['password'].value);
+        console.log(this.loginForm.controls['rememberMe'].value);
+    }
+
+    togglePasswordVisibility() {
+        this.hidePassword.set(!this.hidePassword());
+    }
 }
